172892b5b0a40b6a9cc08a3745ce4a46fbe7c2d3
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2020-09-04 10:55:35 +0200

    feat(app): initialize app

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <div className="header">
+            <header>Data canvas app</header>
+        </div>
+    );
+};
+
+export default Header;
